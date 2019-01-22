0e620bd97e9181ad96f94d6d6ea11d6dd6d74162
Pavel Struhar <pstruhar@gmail.com>
2019-01-23 00:05:01 +0100

    Format by prettier

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { App } from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import { App } from "./App";
 
-describe('<App />', () => {
-  it('matches its snapshot', () => {
+describe("<App />", () => {
+  it("matches its snapshot", () => {
     const component = shallow(<App />);
     expect(component).toMatchSnapshot();
   });
