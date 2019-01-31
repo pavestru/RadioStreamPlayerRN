c0305e158d9d3af36a060005d1e3294086e97cb7
Pavel Struhar <pstruhar@gmail.com>
2019-01-31 23:27:47 +0100

    Style title and artist in Player

--- src/Player.tsx
+++ src/Player.tsx
@@ -15,7 +15,8 @@ const _styles = {
     backgroundColor: "lightgray",
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
-    flexDirection: "row"
+    flexDirection: "row",
+    paddingRight: 15
   }),
   button: RX.Styles.createViewStyle({
     width: 70,
@@ -27,6 +28,18 @@ const _styles = {
   info: RX.Styles.createViewStyle({
     flexShrink: 1,
     flexGrow: 1
+  }),
+  title: RX.Styles.createTextStyle({
+    lineHeight: 24,
+    textAlignVertical: "center",
+    fontSize: 18,
+    marginTop: 24
+  }),
+  artist: RX.Styles.createTextStyle({
+    lineHeight: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "gray"
   })
 };
 
@@ -70,8 +83,12 @@ export class Player extends RX.Component<Props, State> {
           )}
         </RX.GestureView>
         <RX.View style={_styles.info}>
-          <RX.Text>{title}</RX.Text>
-          <RX.Text>{artist}</RX.Text>
+          <RX.Text numberOfLines={1} style={_styles.title}>
+            {title}
+          </RX.Text>
+          <RX.Text numberOfLines={1} style={_styles.artist}>
+            {artist}
+          </RX.Text>
         </RX.View>
       </RX.View>
     );
