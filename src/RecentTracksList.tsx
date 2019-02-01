48e68251893b7dc32de68a8de792005d751d6a71
Pavel Struhar <pstruhar@gmail.com>
2019-02-01 23:59:08 +0100

    Style RecentTracksList

--- src/RecentTracksList.tsx
+++ src/RecentTracksList.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import RX from "reactxp";
 
+const _styles = {
+  main: RX.Styles.createViewStyle({
+    padding: 15
+  }),
+  listItem: RX.Styles.createViewStyle({
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 15
+  }),
+  artistAndTitle: RX.Styles.createViewStyle({}),
+  titleText: RX.Styles.createTextStyle({
+    fontSize: 20
+  }),
+  artistText: RX.Styles.createTextStyle({
+    color: "gray",
+    fontWeight: "bold",
+    fontSize: 18
+  }),
+  timeText: RX.Styles.createTextStyle({
+    height: 25,
+    lineHeight: 25,
+    textAlignVertical: "center",
+    color: "gray",
+    fontSize: 15
+  })
+};
+
 const formatTime = (time: number) => {
   const jsTime = new Date(time * 1000);
   return jsTime.toLocaleTimeString();
@@ -17,18 +44,16 @@ interface Props {
 }
 
 export const RecentTracksList = ({ tracks }: Props) => (
-  <RX.View>
+  <RX.View style={_styles.main}>
     <RX.Text>Posledné hrané:</RX.Text>
     {tracks.map(({ artist, title, time }) => (
-      <RX.View key={time}>
-        <RX.View>
-          <RX.Text>{title}</RX.Text>
+      <RX.View key={time} style={_styles.listItem}>
+        <RX.View style={_styles.artistAndTitle}>
+          <RX.Text style={_styles.titleText}>{title}</RX.Text>
+          <RX.Text style={_styles.artistText}>{artist}</RX.Text>
         </RX.View>
         <RX.View>
-          <RX.Text>{artist}</RX.Text>
-        </RX.View>
-        <RX.View>
-          <RX.Text>od {formatTime(time)}</RX.Text>
+          <RX.Text style={_styles.timeText}>od {formatTime(time)}</RX.Text>
         </RX.View>
       </RX.View>
     ))}
