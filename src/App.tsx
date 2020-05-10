80c3c6e599faba7b1ee398eb70e64c6964d916a9
Pavel Struhar <pavel.struhar@accenture.com>
2020-05-10 23:27:39 +0200

    Upgrade to react-navigation v.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import RX from "reactxp";
 import "react-native-gesture-handler";
-import { createAppContainer } from "react-navigation";
-import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 import { StateContextWrapper } from "./StateContext";
 
@@ -12,56 +12,57 @@ import { Player } from "./Player";
 import { RecentTracksList } from "./RecentTracksList";
 import { Articles } from "./Articles";
 
-const MainNavigator = createMaterialTopTabNavigator(
-  {
-    Články: { screen: Articles },
-    "Posledné hrané": { screen: RecentTracksList }
+const tabBarOptions = {
+  // upperCaseLabel: false,
+  activeTintColor: "#3d6e8d",
+  style: {
+    backgroundColor: "#d5dee7",
   },
-  {
-    tabBarOptions: {
-      upperCaseLabel: false,
-      activeTintColor: "#3d6e8d",
-      style: {
-        backgroundColor: "#d5dee7"
-      },
-      labelStyle: {
-        color: "#3d6e8d",
-        fontSize: 20,
-        fontWeight: "bold"
-      },
-      indicatorStyle: {
-        backgroundColor: "#99a",
-        height: 4,
-        borderTopLeftRadius: 2,
-        borderTopRightRadius: 2
-      }
-    }
-  }
-);
+  labelStyle: {
+    color: "#3d6e8d",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  indicatorStyle: {
+    backgroundColor: "#99a",
+    height: 4,
+    borderTopLeftRadius: 2,
+    borderTopRightRadius: 2,
+  },
+};
 
-const MainView = createAppContainer(MainNavigator);
+const Tab = createMaterialTopTabNavigator();
+
+const MainView = () => (
+  <NavigationContainer>
+    <Tab.Navigator tabBarOptions={tabBarOptions}>
+      <Tab.Screen name="Články" component={Articles} />
+      <Tab.Screen name="Posledné hrané" component={RecentTracksList} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
 
 const _styles = {
   main: RX.Styles.createViewStyle({
     flex: 1,
-    backgroundColor: "#3d6e8d"
+    backgroundColor: "#3d6e8d",
   }),
 
   content: RX.Styles.createViewStyle({
     flexShrink: 1,
-    flexGrow: 1
+    flexGrow: 1,
   }),
 
   logoView: RX.Styles.createViewStyle({
-    backgroundColor: "#fafbfd"
+    backgroundColor: "#fafbfd",
   }),
 
   logo: RX.Styles.createImageStyle({
     marginTop: isIphoneX ? 50 : 0,
     marginHorizontal: 20,
     marginBottom: 10,
-    height: 80
-  })
+    height: 80,
+  }),
 };
 
 export class App extends RX.Component {
